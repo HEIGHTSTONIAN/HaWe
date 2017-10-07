1c6284e2751dfb8866649bd4db09a505b10bd96a
Cassandra Claire <cassandra_villaruel@yahoo.com>
2017-10-07 12:20:54 -0500

    can put sign up user info in database

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,25 @@
 
 var db = require("../models");
-
+var express = require("express");
 
 module.exports = function (app) {
+
+    //Getting User Sign Up info to Database
+    app.post("/login", function(req, res) {
+
+        const username = req.body.username;
+        const email = req.body.email;
+        const password = req.body.email;
+
+        db.User.create({
+            username: username,
+            email: email,
+            password: password
+          }).then(function(dbUser) {
+            console.log("did this2");
+            res.json(dbUser);
+          });
+
+      });
 
 };
