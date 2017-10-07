b1a7a4bcf01dd52b2ccde085af3ddeb09103d75b
Cassandra Claire <cassandra_villaruel@yahoo.com>
2017-10-07 12:58:14 -0500

    added in sign up so they can put user data in database. has authentication so that they are not able to write invalid things

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -15,16 +15,20 @@ module.exports = function (app) {
         const password = req.body.email;
 
         req.checkBody('username', 'Name is required').notEmpty();
+        req.checkBody('username', 'Username must be between 4-15 characters long.').len(4, 15);
         req.checkBody('email', 'Email is required').notEmpty();
         req.checkBody('email', 'Email is not valid').isEmail();
+        req.checkBody('email', 'Email address must be between 4-100 characters long, please try again.').len(4, 100);
         req.checkBody('password', 'Password is required').notEmpty();
+        req.checkBody('password', 'Password must be between 8-100 characters long.').len(8, 100);
+        req.checkBody('passwordMatch', 'Password must be between 8-100 characters long.').len(8, 100);
         req.checkBody('passwordMatch', 'Passwords do not match').equals(req.body.password);
 
         var errors = req.validationErrors();
 
         if(errors){
             console.log(errors);
-            res.render()
+            res.redirect('/login');
         } else {
             db.User.create({
                 username: username,
