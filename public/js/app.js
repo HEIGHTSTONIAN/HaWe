748eea38cd8ce886d13513e40cae042ee139e394
mattsald48 <matt.saldana8@gmail.com>
2017-10-10 13:05:42 -0500

    Tasks will now post onto the server and be displayed on the page

--- public/js/app.js
+++ public/js/app.js
@@ -1,42 +1,53 @@
+//***The code as is will get all task.****  
+//When users table is read we can link them up 
+//so only the logged in users task will show
+
+
 $(document).ready(function() {
-// Getting a reference to the input field where user adds a new todo
-  var $newItemInput = $("#taskName");
-  var $actionTaskAdd = $("#action-task-add");
-  // Our new todos will go inside the todoContainer
-  var $todoContainer = $(".todo-container");
 
-  $(document).on("submit", "#newTask", insertTodo);
 
-  // Our initial todos array
-  var todos = [];
+//Grab html elements in jqeury
+var $newItemInput = $("#taskName");
+var $actionTaskAdd = $("#action-task-add");
 
-  // Getting todos from database when page loads
-  getTodos();
+// Our new todos will go inside the todoContainer
+var $todoContainer = $("#tasks-list");
+//Array of new Inputs
+//var $newInputRow = [];
+//todos array that is used for holding todos to push into the html
+var todos = [];
 
-  // This function resets the todos displayed with new todos from the database
-  // function initializeRows() {
-  //   console.log("In initializeRows");
-  //   $todoContainer.empty();
-  //   var rowsToAdd = [];
-  //   for (var i = 0; i < todos.length; i++) {
-  //     rowsToAdd.push(createNewRow(todos[i]));
-  //   }
-  //   $todoContainer.prepend(rowsToAdd);
-  // }
+$(document).on("submit", "#newTask", insertTodo);
 
-  // This function grabs todos from the database and updates the view
+
+getTodos();// this happens on page load to get the tasks and start working on them
+
+
+//This will empty out the row of task and remake it with a new task added. 
+  function initializeRows() {
+    console.log("Inside of initializeRows");
+    $todoContainer.empty();
+    var rowsToAdd = [];
+    console.log(todos);
+    for (var i = 0; i < todos.length; i++) {
+      rowsToAdd.push(createNewRow(todos[i]));
+      console.log(rowsToAdd);
+    }
+    $todoContainer.prepend(rowsToAdd);
+  }
+
+//this will get the todos out of the db
   function getTodos() {
     console.log("In getTodos");
     $.get("/app/todos", function(data) {
-      console.log("Data in getTodos: "+data);
-      todos = data;
-      console.log("Todos in getTodos: "+todos);
-      //initializeRows();
-    });
-    window.location.href = "/app"
-  }
 
-  // This function updates a todo in our database
+      todos = data;
+
+      initializeRows();  //this will start the process of making rows
+  });
+ }
+
+// This function updates a todo in our database
   function updateTodo(todo) {
     $.ajax({
       method: "PUT",
@@ -45,42 +56,51 @@ $(document).ready(function() {
     }).done(getTodos);
   }
 
-  // This function inserts a new todo into our database and then updates the view
+// This function inserts a new todo into our database and then updates the view
   function insertTodo(event) {
-    //event.preventDefault();
+    event.preventDefault();
     console.log("Inside of insertTodo");
-    console.log("Event: "+event);
-    console.log("$newItemInput: "+$newItemInput);
+    
     var newItem = event;
     var todo = {
       text: $newItemInput.val().trim(),
       complete: false
-    };
-
-    console.log("todo: "+todo);
+   };
+    
     $.post("/app/todos", todo, getTodos);
     //window.location.href = "/app"
     $newItemInput.val("");
+
+  }//end of insert
+
+
+// This function creates the html for a todo task. 
+  function createNewRow(todo) {
+    console.log("Inside of createNewRow")
+     var $newInputRow = $(
+      [
+		"<li>",
+			"<div class='status'>To Do</div>",
+			"<input class='check-status' type='checkbox' name='task[]' />",
+			"<span>", 
+			todo.text, 
+			"</span>",
+		"</li>"
+      ].join("")
+    );
+
+//****Haven't test yet*******I don't think .changeClass will work
+//Marks the task as Done if the todo.complete is true
+    if (todo.complete) {
+      $newInputRow.find("div.status").changeClass("done");
+    }
+
+    return $newInputRow;
   }
 
 
 
 
 
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})//end of document ready
--- routes/todo-routes.js
+++ routes/todo-routes.js
@@ -1,12 +1,12 @@
 var db = require("../models");
 
 
-// Routes
-// =============================================================
+//****Routes will need to be modified when user table is added******
+//*******************************************************************
 module.exports = function(app) {
 
 //GET route for getting all of the todos
-  app.get("/app/todos/", function(req, res) {
+ app.get("/app/todos/", function(req, res) {
 
    db.Todo.findAll({}).then(function(results){
       res.json(results);
@@ -28,31 +28,23 @@ module.exports = function(app) {
  //   });
  // });
 
-//Get route for getting all of the todos of a specific user at a certain date
-//
-//
 
 // POST route for saving a new todo
   app.post("/app/todos/", function(req, res) {
-    // create takes an argument of an object describing the item we want to
-    // insert into our table. In this case we just we pass in an object with a text
-    // and complete property (req.body)
+
     console.log("req.body in app.post: ");
     console.log(req.body);
+    
     db.Todo.create({
-    		text: req.body.taskName,  //was req.body.taskName
-
+    		text: req.body.text, 
+        complete: req.body.complete 
     	}).then(function(dbTodo) {
-      // We have access to the new todo as an argument inside of the callback function
       res.json(dbTodo)
-
     })
-    .catch(function(err) {
-      // Whenever a validation or flag fails, an error is thrown
-      // We can "catch" the error to prevent it from being "thrown", which could crash our node app
+    .catch(function(err) {      
       res.json(err);
     });
-    res.redirect("/app");
+    //res.redirect("/app");
   });
 
 // DELETE route for deleting todos. We can get the id of the todo to be deleted from
@@ -67,27 +59,27 @@ module.exports = function(app) {
   //     res.json(dbTodo);
   //   });
 
-  // });
+// });
 
 // PUT route for updating todos. We can get the updated todo data from req.body
-  app.put("/app/todos/", function(req, res) {
+  // app.put("/app/todos/:id/:id", function(req, res) {
 
-    // Update takes in an object describing the properties we want to update, and
-    // we use where to describe which objects we want to update
-    db.Todo.update({
-      text: req.body.text,
-      complete: req.body.complete
-    }, {
-      where: {
-        id: req.body.id
-      }
-    }).then(function(dbTodo) {
-      res.json(dbTodo);
-    })
-    .catch(function(err) {
-      // Whenever a validation or flag fails, an error is thrown
-      // We can "catch" the error to prevent it from being "thrown", which could crash our node app
-      res.json(err);
-    });
-  });
+  //   // Update takes in an object describing the properties we want to update, and
+  //   // we use where to describe which objects we want to update
+  //   db.Todo.update({
+  //     text: req.body.text,
+  //     complete: req.body.complete
+  //   }, {
+  //     where: {
+  //       id: req.body.id
+  //     }
+  //   }).then(function(dbTodo) {
+  //     res.json(dbTodo);
+  //   })
+  //   .catch(function(err) {
+  //     // Whenever a validation or flag fails, an error is thrown
+  //     // We can "catch" the error to prevent it from being "thrown", which could crash our node app
+  //     res.json(err);
+  //   });
+  // });
 }
