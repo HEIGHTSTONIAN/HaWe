7cc77ee8a270c2551855322d774079a0380f7fe6
Cassandra Claire <cassandra_villaruel@yahoo.com>
2017-10-12 05:57:38 -0500

    task

--- models/todo.js
+++ models/todo.js
@@ -15,7 +15,7 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.DATE,
     }
    });
-<<<<<<< HEAD
+
 
    Todo.associate = function(models) {
     // We're saying that a Post should belong to an Author
@@ -26,15 +26,6 @@ module.exports = function(sequelize, DataTypes) {
       }
     });
   };
-
-=======
-    
-   Todo.associate = function(models){
-     Todo.belongsTo(models.User, {
-      allowNull: true     
-     }); // closes belong to
-   }  
   
->>>>>>> 1ab2b01be5d453324a40641747998e7608667806
   return Todo;
 };
